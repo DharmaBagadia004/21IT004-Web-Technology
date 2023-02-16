e7e27911de30c177f8214d1643349050806cda5a
DharmaBagadia004 <125524527+DharmaBagadia004@users.noreply.github.com>
2023-02-16 07:46:40 +0530

    Add files via upload

---
+++ JS/reverse.js
@@ -0,0 +1,5 @@
+function reverseInput() {
+    var inputBox = document.getElementById("inputBox"); var input = inputBox.value;
+    var reversedInput = input.split('').reverse().join(''); document.getElementById("output").innerHTML = reversedInput;
+    }
+    
